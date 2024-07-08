d0219769c42c5a264ac628df299ee7f0018bd229
Julien Rineau <julien.rineau@berkeley.edu>
2024-07-08 01:28:22 -0700

    Update unet

--- unet.py
+++ unet.py
@@ -1,140 +1,98 @@
+from dataclasses import dataclass, field
+
 import torch
 import torch.nn as nn
-import math
+import torchvision.transforms.functional as TF
 
 
-class Block(nn.Module):
-    """
-    A convolutional block optionally conditioned on external data using FiLM layers.
-    Includes two convolutional layers with ReLU activation and batch normalization.
+@dataclass
+class UnetConfig:
+    in_channels: int = 1
+    out_channels: int = 1
+    features: list[int] = field(default_factory=lambda: [64, 128, 256, 512])
 
-    Parameters:
-    - inputs (int): Number of input channels.
-    - middles (int): Number of middle layer channels.
-    - outs (int): Number of output channels.
-    - use_film (bool): Whether to apply FiLM layers.
-    - film_dim (int): Dimension of the conditioning vector for FiLM layers.
-    """
 
-    def __init__(
-        self,
-        inputs: int = 1,
-        middles: int = 64,
-        outs: int = 64,
-        use_film: bool = False,
-        film_dim: int = None,
-    ):
-        super().__init__()
-        self.use_film = use_film
-        self.conv1 = nn.Conv1d(inputs, middles, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv1d(middles, outs, kernel_size=3, stride=1, padding=1)
-        self.relu = nn.ReLU()
-        self.bn = nn.BatchNorm1d(outs)
-
-        if self.use_film:
-            self.film1 = FiLM(film_dim, middles)
-            self.film2 = FiLM(film_dim, outs)
-
-    def forward(
-        self, x: torch.Tensor, film_params: torch.Tensor = None
-    ) -> torch.Tensor:
-        if self.use_film:
-            x = self.film1(x, film_params)
-        x = self.relu(self.conv1(x))
-        if self.use_film:
-            x = self.film2(x, film_params)
-        x = self.relu(self.bn(self.conv2(x)))
-        return x
-
-class SinusoidalPositionalEncoding(nn.Module):
-    """
-    Generates and applies sinusoidal positional encodings to the input feature maps.
-    """
-    def __init__(self, channels, length):
-        super().__init__()
-        self.encoding = nn.Parameter(self.create_positional_encoding(channels, length), requires_grad=False)
-
-    @staticmethod
-    def create_positional_encoding(channels, length):
-        if channels % 2 == 1:
-            channels -= 1
-
-        position = torch.arange(length).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, channels, 2) * -(math.log(10000.0) / channels))
-        pe = torch.zeros(length, channels)
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-
-        return pe.transpose(0, 1).unsqueeze(0) 
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.GELU(approximate="tanh"),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.GELU(approximate="tanh"),
+        )
 
     def forward(self, x):
-        return x + self.encoding
-
-class FiLM(nn.Module):
-    """
-    Feature-wise Linear Modulation layer to conditionally modulate the feature maps.
-    """
-
-    def __init__(self, film_dim: int, num_features: int):
-        super().__init__()
-        self.scale_transform = nn.Linear(film_dim, num_features)
-        self.shift_transform = nn.Linear(film_dim, num_features)
-
-    def forward(self, x: torch.Tensor, z: torch.Tensor) -> torch.Tensor:
-        scale = self.scale_transform(z).unsqueeze(2)
-        shift = self.shift_transform(z).unsqueeze(2)
-        return x * scale + shift
+        return self.conv(x)
 
 
-class UNet(nn.Module):
-    def __init__(self, film_dim: int, length: int):
-        super().__init__()
-        self.pos_enc = SinusoidalPositionalEncoding(64, length)
-        self.en1 = Block(64, 64, 64, use_film=True, film_dim=film_dim)
-        self.en2 = Block(64, 128, 128, use_film=True, film_dim=film_dim)
-        self.en3 = Block(128, 256, 256, use_film=True, film_dim=film_dim)
-        self.en4 = Block(256, 512, 512, use_film=True, film_dim=film_dim)
-        self.en5 = Block(512, 1024, 512, use_film=True, film_dim=film_dim)
+class UNET(nn.Module):
+    def __init__(self, UnetConfig):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.upsample4 = nn.ConvTranspose1d(
-            512, 512, kernel_size=4, stride=2, padding=1
+        in_channels = UnetConfig.in_channels  # init the first channel
+
+        # Down part
+        for feature in UnetConfig.features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
+
+        for feature in reversed(UnetConfig.features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature * 2,
+                    feature,
+                    kernel_size=2,
+                    stride=2,
+                )
+            )
+            self.ups.append(DoubleConv(feature * 2, feature))
+
+        self.bottleneck = DoubleConv(
+            UnetConfig.features[-1], UnetConfig.features[-1] * 2
         )
-        self.de4 = Block(1024, 512, 256, use_film=True, film_dim=film_dim)
+        self.final_conv = nn.Conv2d(
+            UnetConfig.features[0], UnetConfig.out_channels, kernel_size=1
+        )  # weighted sum across all input channels for each spatial position
 
-        self.upsample3 = nn.ConvTranspose1d(
-            256, 256, kernel_size=4, stride=2, padding=1
-        )
-        self.de3 = Block(512, 256, 128, use_film=True, film_dim=film_dim)
+    def forward(self, x):
+        skip_connections = []
 
-        self.upsample2 = nn.ConvTranspose1d(
-            128, 128, kernel_size=4, stride=2, padding=1
-        )
-        self.de2 = Block(256, 128, 64, use_film=True, film_dim=film_dim)
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
 
-        self.upsample1 = nn.ConvTranspose1d(64, 64, kernel_size=4, stride=2, padding=1)
-        self.de1 = Block(128, 64, 64, use_film=True, film_dim=film_dim)
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]  # take all elements, in reverse order
 
-        self.conv_last = nn.Conv1d(64, 1, kernel_size=1, stride=1, padding=0)
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)  # transposed convolution (upsampling) operation.
+            skip_connection = skip_connections[idx // 2]
 
-    def forward(self, x: torch.Tensor, film_params: torch.Tensor) -> torch.Tensor:
-        x = self.pos_enc(x)
-        x = self.en1(x, film_params)
-        x = self.en2(x, film_params)
-        x = self.en3(x, film_params)
-        x = self.en4(x, film_params)
-        x = self.en5(x, film_params)
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
 
-        x = self.upsample4(x)
-        x = self.de4(x, film_params)
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](concat_skip)  # DoubleConv operation
 
-        x = self.upsample3(x)
-        x = self.de3(x, film_params)
+        logits = self.final_conv(x)
+        return logits
 
-        x = self.upsample2(x)
-        x = self.de2(x, film_params)
 
-        x = self.upsample1(x)
-        x = self.de1(x, film_params)
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
-        x = self.conv_last(x)
-        return x
+    model = UNET(UnetConfig()).to(device)
+    x = torch.randn((8, 1, 28, 28)).to(device)  # Image should be (B, C, W, H)
+
+    with torch.no_grad():
+        preds = model(x)
+
+    print(f"Input shape: {x.shape}")
+    print(f"Output shape: {preds.shape}")
